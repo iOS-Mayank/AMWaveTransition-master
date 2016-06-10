0f6e6907c77d7589601eec0553f6447f768eb639
iOS-Mayank <Sonitek@Sonitek-Macs-iMac.local>
2016-06-10 18:09:29 +0530

    TabeleView-WaveTrabsition-In-ObjC
    TabeleView-WaveTrabsition-In-ObjC

---
+++ AMWaveTransition/AMWaveTransition.h
@@ -0,0 +1,19 @@
+//
+//  AMWaveTransition.h
+//  AMWaveTransition
+//
+//  Created by Andrea Mazzini on 14/08/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMWaveTransition.
+FOUNDATION_EXPORT double AMWaveTransitionVersionNumber;
+
+//! Project version string for AMWaveTransition.
+FOUNDATION_EXPORT const unsigned char AMWaveTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMWaveTransition/PublicHeader.h>
+#import <AMWaveTransition/AMWaveTransition.h>
+#import <AMWaveTransition/AMWaveViewController.h>
